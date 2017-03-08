d0dc5af11009572eff2b84d393bf678a6a96a52c
patres2 <res17271@spengergasse.at>
2017-03-08 19:01:22 +0100

    Anlage_Company

---
+++ Git_Hub_Eclipse/src/model/Customer.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Customer
+{
+	private String name;
+	
+	public Customer(String name)
+	{
+		setName("Patrick");
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+}
